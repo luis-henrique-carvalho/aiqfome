fec32dc35a8ee8b361023c91c858e4966df3fc1a
luis-henrique-carvalho <luisdev9225@gmail.com>
2025-05-27 20:54:23 -0300

    feat: add NavBar component and update layout with outdoor image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,14 @@
 import "./globals.css";
 
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Nunito } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import NavBar from "@/components/layout/NavBar";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const nunito = Nunito({
+  variable: "--font-nunito",
   subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 export const metadata: Metadata = {
@@ -25,9 +23,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${nunito.variable} w-full antialiased`}>
+        <NavBar />
         {children}
       </body>
     </html>
